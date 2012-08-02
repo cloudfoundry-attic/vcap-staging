be5b87aeb5d0cd532d523cf6556d5fc94c298131
Glenn Oppegard <oppegard@gmail.com>
2012-08-01 18:47:45 -0700

    include latest cf-autoconfig and update cf-runtime refs to 0.0.2
    cf-runtime: 0.0.2 replaces crack gem dependency with okjson.rb for JSON parsing
    
    cf-autoconfig: 0.0.4 bumps cf-runtime dependency to 0.0.2 and include changes
     from vcap-ruby commit 3ba209c, which uses Gem.loaded_specs for service
     library version
    
    ruby_autoconfig.rb: bump cf-runtime and cf-autoconfig version checks and
     remove check for crack gem
    
    Change-Id: I6a9693987f6608c8ea0bb44af458c3730027540e

--- spec/unit/gemfile_support_spec.rb
+++ spec/unit/gemfile_support_spec.rb
@@ -14,7 +14,7 @@ describe 'An app that uses Bundler being staged' do
   it 'is packaged with all gems excluding those in test group' do
     stage :sinatra do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0","json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "redis-3.0.1",
             "rubyzip-0.9.9", "sinatra-1.2.3", "thin-1.2.11", "thor-0.15.3", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -33,7 +33,7 @@ BUNDLE_WITHOUT: test
     it 'is packaged with gems only in groups specified by BUNDLE_WITHOUT' do
        stage(:sinatra, {:environment=>["BUNDLE_WITHOUT=development"]}) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3", "diff-lcs-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3", "diff-lcs-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0", "json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "rspec-2.11.0", "rspec-core-2.11.0",
             "rspec-expectations-2.11.1", "rspec-mocks-2.11.1", "sinatra-1.2.3", "thin-1.2.11", "thor-0.15.3", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -53,7 +53,7 @@ BUNDLE_WITHOUT: development
     it 'is packaged with gems only in groups specified by BUNDLE_WITHOUT' do
        stage(:sinatra, {:environment=>["BUNDLE_WITHOUT=development:assets"]}) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3", "diff-lcs-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3", "diff-lcs-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0", "json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "rspec-2.11.0", "rspec-core-2.11.0",
             "rspec-expectations-2.11.1", "rspec-mocks-2.11.1", "sinatra-1.2.3", "thin-1.2.11", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -73,7 +73,7 @@ BUNDLE_WITHOUT: development:assets
     it 'is packaged with gems only in groups specified by BUNDLE_WITHOUT' do
        stage(:sinatra, {:environment=>["BUNDLE_WITHOUT=development: assets"]}) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3", "diff-lcs-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3", "diff-lcs-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0", "json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "rspec-2.11.0", "rspec-core-2.11.0",
             "rspec-expectations-2.11.1", "rspec-mocks-2.11.1", "sinatra-1.2.3", "thin-1.2.11", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -94,7 +94,7 @@ BUNDLE_WITHOUT: development: assets
     it 'is packaged with all gems' do
       stage(:sinatra, {:environment=>["BUNDLE_WITHOUT="]}) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3", "diff-lcs-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3", "diff-lcs-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0", "json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "redis-3.0.1", "rspec-2.11.0", "rspec-core-2.11.0",
             "rspec-expectations-2.11.1", "rspec-mocks-2.11.1", "rubyzip-0.9.9", "sinatra-1.2.3", "thin-1.2.11", "thor-0.15.3", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -138,7 +138,7 @@ describe 'An app being staged that contains gems with valid local file paths tha
   it 'is packaged with the local vendored gems' do
     stage(:sinatra) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["broken-0.0.1","bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1", "rack-1.4.1",
+         Dir.glob('*').sort.should == ["broken-0.0.1","bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2", "rack-1.4.1",
            "rack-protection-1.2.0", "sinatra-1.3.2", "tilt-1.3.3"]
        end
     end
@@ -186,7 +186,7 @@ describe 'An app being staged containing a gem designated for a specific Ruby pl
   it 'is packaged without the gem if ruby version does not match' do
     stage(:sinatra, {:runtime => "ruby19"}) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.9.1','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0","json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "redis-3.0.1",
             "rubyzip-0.9.9", "sinatra-1.2.3", "thin-1.2.11", "thor-0.15.3", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -196,7 +196,7 @@ describe 'An app being staged containing a gem designated for a specific Ruby pl
   it 'is packaged with the gem if ruby version matches' do
     stage(:sinatra) do |staged_dir|
        Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-         Dir.glob('*').sort.should == ["bundler-1.1.3","carrot-1.2.0","cf-autoconfig-0.0.3","cf-runtime-0.0.1","crack-0.3.1","daemons-1.1.3",
+         Dir.glob('*').sort.should == ["bundler-1.1.3","carrot-1.2.0","cf-autoconfig-0.0.4","cf-runtime-0.0.2","daemons-1.1.3",
             "eventmachine-0.12.10","execjs-1.4.0","json-1.5.1","multi_json-1.3.6","rack-1.2.2", "rake-0.8.7", "redis-3.0.1",
             "rubyzip-0.9.9", "sinatra-1.2.3", "thin-1.2.11", "thor-0.15.3", "tilt-1.3", "uglifier-1.2.6"]
        end
@@ -214,7 +214,7 @@ describe 'An app being staged with a Gemfile.lock created on Windows' do
     # Verify eventmachine version does not contain x86-mingw32
     stage :sinatra do |staged_dir|
       Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-        Dir.glob('*').sort.should == ["bundler-1.1.3", "cf-autoconfig-0.0.3", "cf-runtime-0.0.1", "crack-0.3.1",
+        Dir.glob('*').sort.should == ["bundler-1.1.3", "cf-autoconfig-0.0.4", "cf-runtime-0.0.2",
           "daemons-1.1.8", "eventmachine-1.0.0.rc.4", "json-1.7.3", "rack-1.4.1", "rack-protection-1.2.0",
           "sinatra-1.3.2", "thin-1.4.1", "tilt-1.3.3"]
       end
@@ -226,7 +226,7 @@ describe 'An app being staged with a Gemfile.lock created on Windows' do
       # Verify we don't install mysql2 gem
       stage :sinatra do |staged_dir|
         Dir.chdir(File.join(staged_dir,'app', 'rubygems', 'ruby', '1.8','gems')) do
-          Dir.glob('*').sort.should == ["bundler-1.1.3", "cf-autoconfig-0.0.3", "cf-runtime-0.0.1", "crack-0.3.1",
+          Dir.glob('*').sort.should == ["bundler-1.1.3", "cf-autoconfig-0.0.4", "cf-runtime-0.0.2",
             "daemons-1.1.8", "eventmachine-1.0.0.rc.4", "json-1.7.3", "rack-1.4.1", "rack-protection-1.2.0",
             "sinatra-1.3.2", "thin-1.4.1", "tilt-1.3.3"]
         end
