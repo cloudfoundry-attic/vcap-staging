c4d79d03a9cd3b0b6e8f6e085fb83a63e5c56f18
Maria Shaldybina and Max Brunsfeld <mariash@vmware.com>
2013-01-04 11:37:08 -0800

    Allow start command to be specified in manifest.yml
    Also, remove specs for ruby and node build packs
    In buildpack specs, use dummy buildpacks
    
    Change-Id: I4f6d173ff0419764606546c3ded0e459b6dcdabd

--- spec/unit/buildpack_spec.rb
+++ spec/unit/buildpack_spec.rb
@@ -1,37 +1,49 @@
 require 'spec_helper'
+require 'vcap/staging/plugin/buildpack/plugin'
 
 describe "Buildpack Plugin" do
-  it "stages a rails 3 app" do
-    app_fixture :rails3_with_ruby_version
+  let(:fake_buildpacks_dir) { File.expand_path("../../fixtures/fake_buildpacks", __FILE__) }
+  let(:buildpacks_path_with_start_cmd) { "#{fake_buildpacks_dir}/with_start_cmd" }
+  let(:buildpacks_path_without_start_cmd) { "#{fake_buildpacks_dir}/without_start_cmd" }
+  let(:buildpacks_path_with_no_match) { "#{fake_buildpacks_dir}/with_no_match" }
 
-    stage buildpack_staging_env do |staged_dir|
-      copies_app_to_destination?(staged_dir)
-      creates_vendor_ruby_dir(staged_dir)
-      captures_output_to_the_staging_log(staged_dir)
-      stores_everything_in_profile(staged_dir)
-      packages_with_start_script(staged_dir)
+  let(:buildpacks_path) { buildpacks_path_with_start_cmd }
+  let(:app_with_procfile) { :node_deps_native }
+  let(:app_without_procfile) { :node_without_procfile }
+
+  before do
+    BuildpackPlugin.any_instance.stub(:buildpacks_path).and_return(Pathname.new(buildpacks_path))
+  end
+
+  shared_examples_for "successful buildpack compilation" do
+    it "copies the app directory to the correct destination" do
+      stage staging_env do |staged_dir|
+        File.should be_file("#{staged_dir}/app.js")
+      end
     end
-  end
 
-  def copies_app_to_destination?(staged_dir)
-    File.should be_file("#{staged_dir}/app/controllers/application_controller.rb")
-  end
+    it "captures output to the staging log" do
+      stage staging_env do |staged_dir|
+        staging_log = File.join(staged_dir, 'logs', 'staging.log')
+        staging_log_body = File.read(staging_log)
+        staging_log_body.should include("-----> Some compilation output")
+      end
+    end
 
-  def captures_output_to_the_staging_log(staged_dir)
-    staging_log = File.join(staged_dir, 'logs', 'staging.log')
-    staging_log_body = File.read(staging_log)
-    staging_log_body.should include("-----> Writing config/database.yml to read from DATABASE_URL")
-  end
+    it "puts the environment variables provided by 'release' into the startup script" do
+      stage staging_env do |staged_dir|
+        start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
+        script_body = File.read(start_script)
+        script_body.should include('export FROM_BUILD_PACK="${FROM_BUILD_PACK:-yes}"')
+      end
+    end
 
-  def creates_vendor_ruby_dir(staged_dir)
-    File.should be_directory("#{staged_dir}/vendor/ruby-1.9.2")
-  end
-
-  def stores_everything_in_profile(staged_dir)
-    start_script = File.join(staged_dir, '.cloudfoundry','startup')
-    start_script.should be_executable_file
-    script_body = File.read(start_script)
-    script_body.should include(<<-EXPECTED)
+    it "stores everything in profile" do
+      stage staging_env do |staged_dir|
+        start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
+        start_script.should be_executable_file
+        script_body = File.read(start_script)
+        script_body.should include(<<-EXPECTED)
 if [ -d .profile.d ]; then
   for i in .profile.d/*.sh; do
     if [ -r $i ]; then
@@ -40,84 +52,85 @@ if [ -d .profile.d ]; then
   done
   unset i
 fi
-    EXPECTED
-  end
-
-  def packages_with_start_script(staged_dir)
-    start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
-    start_script.should be_executable_file
-    script_body = File.read(start_script)
-    script_body.should include("bundle exec thin start -R config.ru -e $RAILS_ENV -p $PORT > $DROPLET_BASE_DIR/logs/stdout.log 2> $DROPLET_BASE_DIR/logs/stderr.log &")
-  end
-
-  it "puts the environment variables provided by 'release' into the startup script" do
-    app_fixture :rails3_with_ruby_version
-
-    stage buildpack_staging_env([postgres_service]) do |staged_dir|
-      start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
-      script_body = File.read(start_script)
-      script_body.should include('export GEM_PATH="${GEM_PATH:-vendor/bundle/ruby/1.9.1}"')
-      script_body.should include('export RAILS_ENV')
-      script_body.should include('export PORT="$VCAP_APP_PORT"')
-      script_body.should include('export DATABASE_URL="postgres://testuser:test@myhost:345/mydb"')
-      script_body.should include('export TMPDIR="$PWD/.cloudfoundry/tmp"')
-    end
-  end
-
-  context "with a node app" do
-    before do
-      app_fixture :node_deps_native
-    end
-
-    it "gives it a start command" do
-      stage buildpack_staging_env do |staged_dir|
-        start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
-        start_script.should be_executable_file
-        script_body = File.read(start_script)
-        script_body.should include("node app.js > $DROPLET_BASE_DIR/logs/stdout.log 2> $DROPLET_BASE_DIR/logs/stderr.log &")
+        EXPECTED
       end
     end
   end
 
-  context "when staging an app which does not match any build packs" do
-    it "raises an error" do
-      app_fixture :phpinfo
+  let(:staging_env) { buildpack_staging_env }
 
-      expect { stage buildpack_staging_env }.to raise_error "Unable to detect a supported application type"
+  context "when a start command is passed" do
+    let(:staging_env) { buildpack_staging_env.merge({:meta => {:command => "node app.js --from-manifest=true"}}) }
+
+    before { app_fixture app_without_procfile }
+
+    it_behaves_like "successful buildpack compilation"
+
+    it "uses the passed start command" do
+      stage staging_env do |staged_dir|
+        packages_with_start_script(staged_dir, "node app.js --from-manifest=true")
+      end
     end
   end
 
-  describe "Procfile support" do
-    it "uses the 'web' process start command" do
-      app_fixture :node_deps_native
-      stage buildpack_staging_env do |staged_dir|
-        start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
-        start_script.should be_executable_file
-        script_body = File.read(start_script)
-        script_body.should include("node app.js > $DROPLET_BASE_DIR/logs/stdout.log 2> $DROPLET_BASE_DIR/logs/stderr.log &")
-      end
-    end
+  context "when the application has a procfile" do
+    before { app_fixture app_with_procfile }
 
-    it "raises an error if the buildpack does not provide a default start command and there is no procfile" do
-      app_fixture :node_without_procfile
-      expect { stage buildpack_staging_env }.to raise_error("Please specify a web start command using a Procfile")
+    it_behaves_like "successful buildpack compilation"
+
+    it "uses the start command specified by the 'web' key in the procfile" do
+      stage staging_env do |staged_dir|
+        packages_with_start_script(staged_dir, "node app.js --from-procfile=true")
+      end
     end
 
     it "raise a good error if the procfile is not a hash" do
       app_fixture :node_invalid_procfile
-      expect { stage buildpack_staging_env }.to raise_error("Invalid Procfile format.  Please ensure it is a valid YAML hash")
+      expect {
+        stage staging_env
+      }.to raise_error("Invalid Procfile format.  Please ensure it is a valid YAML hash")
     end
   end
 
-  def postgres_service
-    {:label=>"postgresql-9.0",
-      :credentials=> {
-          :hostname=>"myhost",
-          :user=>"testuser",
-          :port=>345,
-          :password=>"test",
-          :name=>"mydb"}
-    }
+  context "when no start command is passed and the application does not have a procfile" do
+    before { app_fixture app_without_procfile }
+
+    context "when the buildpack provides a default start command" do
+      it_behaves_like "successful buildpack compilation"
+
+      it "uses the default start command" do
+        stage staging_env do |staged_dir|
+          packages_with_start_script(staged_dir, "node app.js --from-buildpack=true")
+        end
+      end
+    end
+
+    context "when the buildpack does not provide a default start command" do
+      let(:buildpacks_path) { buildpacks_path_without_start_cmd }
+
+      it "raises an error " do
+        expect {
+          stage staging_env
+        }.to raise_error("Please specify a web start command in your manifest.yml or Procfile")
+      end
+    end
+
+    context "when staging an app which does not match any build packs" do
+      let(:buildpacks_path) { buildpacks_path_with_no_match }
+
+      it "raises an error" do
+        expect {
+          stage staging_env
+        }.to raise_error("Unable to detect a supported application type")
+      end
+    end
+  end
+
+  def packages_with_start_script(staged_dir, start_command)
+    start_script = File.join(staged_dir, '.cloudfoundry', 'startup')
+    start_script.should be_executable_file
+    script_body = File.read(start_script)
+    script_body.should include("#{start_command} > $DROPLET_BASE_DIR/logs/stdout.log 2> $DROPLET_BASE_DIR/logs/stderr.log &")
   end
 
   def buildpack_staging_env(services=[])
@@ -126,7 +139,6 @@ fi
         :version => "1.8.7",
         :description => "Ruby 1.8.7",
         :executable => "/usr/bin/ruby",
-        :bundler => "/usr/bin/bundle",
         :environment => {"bundle_gemfile"=>nil}
     },
      :framework_info => {
